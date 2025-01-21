45788e51855e81d5c1b06e537c4f34568809a012
Romain Basly <romain.basly@protonmail.com>
2025-01-21 16:21:40 +0100

    Squashed 'websocket/' changes from d35b2a2..6d73e89
    6d73e89 fix the issues when login and logout
    4505148 add errors logs just incase
    332cb65 remove the wrong path of my .env
    039b20b add further elements to debug the non connexion between the backend and my Websocket server
    38b7c16 add a log to debug the deployment of redis
    5bc26e9 resolve conflicts
    ee4c835 switch to redis for the pubsub
    afc9786 add the keyFileName for prod too, another attempt to make it work
    66789b2 add .env file in the cosystem config file
    c917b0b add another log just in case to see if it is rightly taken into account
    bf3f2fc move the .env file for the config to connect WS with PUBSUB
    40bba3e try to debug the connection with pubsub
    20f614b suppress key file for production keys
    1738245 finish refacto to increment pubsub instead of WS alone
    da73e22 refacto for the use of list items change + modification of flow
    939ac88 initiate exchanges between the backend and the websocket using pub/Sub
    
    git-subtree-dir: websocket
    git-subtree-split: 6d73e8984719de208e5b0768064ba5a472c1792c

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,18 @@ module.exports = {
       name: "WS-PROD",
       script: "./dist/index.js",
       env: {
+        NODE_ENV: "development",
         PORT: 3001,
         CORS_ORIGINS:
           "https://data-list-collaborative-r54h7zfc9-romainbaslys-projects.vercel.app/,https://stingray-app-69yxe.ondigitalocean.app/api",
+        GOOGLE_APPLICATION_CREDENTIALS: "/etc/gcp/key-pub-sub.json",
       },
       env_production: {
         NODE_ENV: "production",
         PORT: 3001,
         CORS_ORIGINS:
           "https://data-list-collaborative-r54h7zfc9-romainbaslys-projects.vercel.app/,https://stingray-app-69yxe.ondigitalocean.app/api",
+        GOOGLE_APPLICATION_CREDENTIALS: "/etc/gcp/key-pub-sub.json",
       },
     },
   ],
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,45 @@
-import dotenv from "dotenv";
-import { SocketService } from "./services/websocket";
 import { SocketConfig } from "./config";
+import { PubSubService } from "./services/pubsub";
+import { SocketService } from "./services/websocket";
 
-dotenv.config();
+require("dotenv").config();
 
 class Main {
   public async start() {
     console.info("socket started");
     const { io, httpServer } = SocketConfig.getInstance().startServer();
-    SocketService.getInstance(io, httpServer);
+    const pubSubService = PubSubService.getInstance(io, httpServer);
+    pubSubService.init();
+
+    const socketService = SocketService.getInstance(io, httpServer);
+    this.registerPubSubListener(pubSubService, socketService);
+
+    process.on("uncaughtException", (err) => {
+      console.error("Uncaught Exception:", err);
+    });
+
+    process.on("unhandledRejection", (reason) => {
+      console.error("Unhandled Rejection:", reason);
+    });
+  }
+
+  private registerPubSubListener(
+    pubSubService: PubSubService,
+    socketService: SocketService
+  ) {
+    const events = [
+      "add_item",
+      "update_item_content",
+      "delete_item",
+      "change_item_status",
+      "new_list_invitation",
+      "list_invitation_status_change",
+      "list_creation",
+    ];
+
+    events.forEach((event) => {
+      socketService.registerListeners(pubSubService, event);
+    });
   }
 }
 
